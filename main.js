4092bbdc93ad673b433ef0cf8d0fa295b3d7577e
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-06-29 20:37:11 -0500

    feat: Add basic Express server with middleware
    The server is currently using `dotenv`, `morgan`, `passport`, `express`,
    `express-session`, and `connect-sqlite3`.
    
    Also `path`, but that isn't an external module.

---
+++ main.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+var express = require('express');
+var morgan = require('morgan');
+var passport = require('passport');
+var path = require('path');
+var session = require('express-session');
+
+var connect = require('connect-sqlite3')(session);
+
+// Middleware
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: new connect({ db: 'sessions.db', dir: './var/' })
+}));
+app.use(passport.authenticate('session'));
+
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}.`));
