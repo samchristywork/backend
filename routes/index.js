e5c133c1a8c0a4ea36401d83d2b3c59ddbdc5dc6
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-06-30 21:24:53 -0500

    feat: Add basic getter and setter routes
    These routes are more here to test that persistent data in the database
    works.

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,23 @@ router.get('/home', function(_req, res, _next) {
   res.sendFile(path.join(__dirname, '../public', 'home.html'));
 });
 
+router.get('/set', function(req, res, next) {
+  var name = req.query.name;
+  if (!name) {
+    return res.send('missing name parameter');
+  }
+
+  db.run('INSERT INTO foo (name) VALUES (?)', [name], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/');
+  });
+});
+
+router.get('/get', function(_req, res, next) {
+  db.all('SELECT * FROM foo', function(err, rows) {
+    if (err) { return next(err); }
+    res.send(rows);
+  });
+});
+
 module.exports = router;
